1dfa3067b2c9a31758d3b83b042e4241925c22df
Soundariya0104 <soundariabala@gmail.com>
2017-01-04 23:10:45 +0530

    Armstrong and Static Programs

---
+++ static/src/StaticEX.java
@@ -0,0 +1,20 @@
+
+public class StaticEX {
+		 
+        public static void main(String[] args) {
+               
+                int result = MathUtility.add(1, 2);
+                System.out.println("(1+2) is : " + result);
+        }
+       
+}
+ 
+class MathUtility{
+         
+        public static int add(int first, int second)
+        {
+                return first + second;
+        }
+ 
+}      
+
